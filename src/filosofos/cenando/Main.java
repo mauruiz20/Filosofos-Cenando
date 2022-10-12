d1251c8b9a3a8c560cf50971353d2ebf8b4a0d9c
Mauricio Ruiz <ruizmauricio012@gmail.com>
2022-10-12 19:24:54 -0300

    Fin

--- src/filosofos/cenando/Main.java
+++ src/filosofos/cenando/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package filosofos.cenando;
 
 /**
@@ -15,7 +10,13 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Mesa m = new Mesa(5);
+        for (int i = 1; i <= 5; i++) {
+            Filosofo f = new Filosofo(m, i);
+            
+            f.start();
+        }
     }
+    
     
 }
